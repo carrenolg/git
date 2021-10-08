8066d9661c9d69386518e3e229ff223f42710055
Luis G. Carreno <carrenolg@gmail.com>
2021-10-08 09:43:42 -0500

    cmd file to repo

---
+++ cmd/cmd.sh
@@ -0,0 +1,11 @@
+# Video 1
+# instalar y añadir identificación
+$ git config --global user.name "Luis G. Carreno"
+$ git config --global user.email "carrenolg@gmail.com"
+
+# Video 4
+# explicación ilustrativa
+
+# Video 5
+$ git init # crea carpeta .git
+
