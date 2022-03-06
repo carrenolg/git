c3193eb8e72452c6eda88d94c5d73d6adeb3c9ac
carrenolg <carrenolg@gmail.com>
2022-03-06 08:00:30 -0500

    diff and patch

---
+++ course/01-introduction-git-github/week1/doch.sh
@@ -0,0 +1,6 @@
+# 01 diff
+$ diff file1.py file2.py
+# create a diff file
+$ diff cpu_usage.py cpu_usage_fixed.py > cpu_usage.diff
+# apply changes to original file
+$ patch cpu_usage.py < cpu_usage.diff
