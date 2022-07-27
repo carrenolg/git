f1d1552d6824fc507c4aeb339473765c671239b0
Luis Carreno <carrenolg@gmail.com>
2022-07-27 15:08:00 -0500

    add new file

---
+++ courses/01-introduction-git-github/week2/008-new-branch/doc.sh
@@ -0,0 +1,8 @@
+# Video: Creating new branches
+$ git branch [name-branch] # create, list, delete
+$ git checkout [name-branch] # change to other branch
+$ git checkout -b [name-branch] # create and switch to new branch
+
+# Video: Working With Branches
+$ git checkout [name-branch] # switch between branches
+$ git branch - d [name-branch] # delete branche
