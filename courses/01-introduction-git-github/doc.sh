edea662799a141dc34944784bcfcade90fa3d473
Luis Carreno <carrenolg@gmail.com>
2022-09-13 08:27:43 -0500

    working with remote repo

---
+++ courses/01-introduction-git-github/doc.sh
@@ -0,0 +1,10 @@
+# Week 3 - Working with remotes
+# Using a remote repository
+$ git remote -v
+$ git remote show origin
+$ git branch -r
+# video: Fetching new changes
+$ git fetch # get commits from origin
+$ git merge origin/master # merge remote changes with local branch
+# video: Updating the Local Repository
+$ git pull
